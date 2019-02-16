b91b93fd28e8bb9e75b9c023141c06b152e6624c
teenarr <tina.chan91@gmail.com>
2019-02-15 17:59:11 -0700

    Got save functioning

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,40 +12,53 @@ var cheerio = require("cheerio");
 module.exports = function (app) {
 
   app.get("/scrape", function (req, res) {
-    // First, we grab the body of the html with axios
-    axios.get("https://www.teenvogue.com/news-politics").then(function (response) {
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(response.data);
+    db.Article.deleteMany({ saved: false }).then(
+      // First, we grab the body of the html with axios
+      axios.get("https://www.teenvogue.com/news-politics").then(function (response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
 
-      // Now, we grab every h2 within an article tag, and do the following:
-      $(".feature-item-content").each(function (i, element) {
-        // Save an empty result object
-        var result = {};
+        // Now, we grab every h2 within an article tag, and do the following:
+        $(".feature-item-content").each(function (i, element) {
+          // Save an empty result object
+          var result = {};
 
-        // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
-          .children().children("h3").text();
-        result.summary = $(this)
-          .children("p").text();
-        result.link = $(this)
-          .children("a")
-          .attr("href");
+          // Add the text and href of every link, and save them as properties of the result object
+          result.title = $(this)
+            .children().children("h3").text();
+          result.summary = $(this)
+            .children("p").text();
+          result.link = $(this)
+            .children("a")
+            .attr("href");
 
-        // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-          .then(function (dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function (err) {
-            // If an error occurred, log it
-            console.log(err);
-          });
+          // Create a new Article using the `result` object built from scraping
+          db.Article.create(result)
+            .then(function (dbArticle) {
+              // View the added result in the console
+              console.log(dbArticle);
+            })
+            .catch(function (err) {
+              // If an error occurred, log it
+              console.log(err);
+            });
+        });
+
+        // Redirect back to home
+        res.redirect("./")
+      })
+    )
+  });
+
+  app.post("/clear", function (req, res) {
+    db.Article.deleteMany({ saved: false })
+      .then(function (dbArticles) {
+        res.json(dbArticles);
+      })
+      .catch(function (err) {
+        res.json(err);
       });
-
-      // Send a message to the client
-      res.send("Scrape Complete");
-    });
+    res.redirect("./")
   });
 
   // Route for getting all Articles from the db
@@ -54,6 +67,18 @@ module.exports = function (app) {
     db.Article.find({})
       .then(function (dbArticle) {
         res.json(dbArticle);
+      })
+      .catch(function (err) {
+        res.json(err);
+      })
+  });
+
+  app.post("/articles/save/:id", function (req, res) {
+    // TODO
+    // ====
+    db.Article.updateOne({ _id: req.params.id }, { saved: true })
+      .then(function (dbArticle) {
+        res.json(dbArticle)
       })
       .catch(function (err) {
         res.json(err);
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,31 +4,15 @@ var db = require("../models");
 module.exports = function (app) {
   app.get("/", function (req, res) {
     db.Article.find({ saved: false }).then(function (dbArticles) {
-      console.log(dbArticles);
-      res.render("index", { articles: dbArticles });
-    });
+        console.log(dbArticles);
+        res.render("index", { articles: dbArticles });
+      });
   });
 
   app.get("/savedArticles", function (req, res) {
-    db.Article.find({ saved: true }).then(function (err, dbArticles) {
-      if (err) {
-        console.log(err);
-      } else {
+    db.Article.find({ saved: true }).then(function (dbArticles) {
         console.log(dbArticles);
         res.render("savedArticles", { articles: dbArticles });
-      }
     })
-  });
-
-  app.get("*", function (req, res) {
-    db.Article.find({ saved: false }).then(
-      function (err, dbArticles) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(dbArticles);
-          res.render("index", { articles: dbArticles });
-        }
-      });
   });
 };
